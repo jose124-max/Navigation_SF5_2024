8c97a8d2f90ad3044ec68307bcfe07e47f3796a1
Jose Alvarez <jalvarezm7@uteq.edu.ec>
2024-01-25 04:13:10 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Navigation_SF5_2024"
+include(":app")
+ 
